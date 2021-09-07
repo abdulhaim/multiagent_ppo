7aa1f2963a7e5844f4b8618fa6d037746cae053b
Marwa Abdulhai <marwaabdulhai@hotmail.com>
2021-09-07 09:00:41 -0700

    make changes for simple spread

--- onpolicy/algorithms/utils/popart.py
+++ onpolicy/algorithms/utils/popart.py
@@ -61,13 +61,13 @@ class PopArt(torch.nn.Module):
         self.mean_sq.mul_(self.beta).add_(batch_sq_mean * (1.0 - self.beta))
         self.debiasing_term.mul_(self.beta).add_(1.0 * (1.0 - self.beta))
 
-        self.stddev = (self.mean_sq - self.mean ** 2).sqrt().clamp(min=1e-4)
+        self.stddev = torch.nn.Parameter((self.mean_sq - self.mean ** 2).sqrt().clamp(min=1e-4))
         
         new_mean, new_var = self.debiased_mean_var()
         new_stddev = torch.sqrt(new_var)
         
-        self.weight = self.weight * old_stddev / new_stddev
-        self.bias = (old_stddev * self.bias + old_mean - new_mean) / new_stddev
+        self.weight = torch.nn.Parameter(self.weight * old_stddev / new_stddev)
+        self.bias = torch.nn.Parameter((old_stddev * self.bias + old_mean - new_mean) / new_stddev)
 
     def debiased_mean_var(self):
         debiased_mean = self.mean / self.debiasing_term.clamp(min=self.epsilon)
